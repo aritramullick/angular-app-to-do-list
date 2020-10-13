8e9936887ecdcd84efda7500d145867ea65adc4a
“aritramullick” <aritramullick@g.ucla.edu>
2020-10-14 02:57:07 +0400

    Commit with initialization of Firebase features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'first-angular-app'),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyBa4Mg1oHeeNJrmYtF90kboxyWrwE7bod0",
+  authDomain: "angular-d9f4b.firebaseapp.com",
+  databaseURL: "https://angular-d9f4b.firebaseio.com",
+  projectId: "angular-d9f4b",
+  storageBucket: "angular-d9f4b.appspot.com",
+  messagingSenderId: "835324098284",
+  appId: "1:835324098284:web:f661edf720c0ae9d9317c9"
+  }
 };
 
 /*
