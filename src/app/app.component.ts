c8f8165694e94c09831e75e7341eaec575bad01b
“aritramullick” <aritramullick@g.ucla.edu>
2020-10-14 15:22:21 +0400

    Added feature of adding tasks to the database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,26 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
 import { Observable } from 'rxjs';
-import { JsonPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  // template: `
-  //   <li *ngFor="let item of items | async">
-  //      {{ item | json }}
-  //   </li>
-  // `,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   todos = [];
   items: Observable<any[]>;
+  reference;
   constructor(db: AngularFireDatabase) {
+    this.reference = db.list('/');
     db.list('/').valueChanges().subscribe((value) => {
       value.forEach((ele) => {
         this.todos.push(ele);
       })
       console.log(this.todos);
     })
-
-    // this.items.forEach((ele) => {
-    //   this.todos.push(ele);
-    //   console.log(ele);
-    // });
   }
   
   title = "first-angular-app";
@@ -36,14 +28,14 @@ export class AppComponent {
   addTodo(todoLabel) {
     if (todoLabel === "")
       return;
-    
-    this.todos.push({
-      label: todoLabel,
-      done: false
-    });
+    const itemsRef = this.reference;
+    itemsRef.push({ label: todoLabel });
   }
   
   deleteTodo(todo) {
+// to get a key, check the Example app below
+    // db.list('/', ref => ref.orderByChild('size').equalTo('large'))
+    // this.reference.remove('');
     this.todos = this.todos.filter( t => t.label !== todo.label);
   }
 }
